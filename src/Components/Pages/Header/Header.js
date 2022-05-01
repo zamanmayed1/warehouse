78b396aa7efea9ae9fd7fb10270c1a0398cd7555
zamanmayed1 <zamanmayed02@gmail.com>
2022-05-02 02:22:55 +0600

    Responsive Header

--- src/Components/Pages/Header/Header.js
+++ src/Components/Pages/Header/Header.js
@@ -1,43 +1,99 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../../Firebase/Firebase.init';
-import CustomLink from '../../Shared/CustomLink';
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import CustomLink from '../../../Components/Shared/CustomLink'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../../Firebase/Firebase.init'
+import { signOut } from 'firebase/auth'
 
-const Header = () => {
+
+
+
+
+export default function Header() {
     const [user] = useAuthState(auth)
     return (
-        <nav className="relative w-full flex items-center justify-between py-4 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
-            <div className="container-fluid w-full md:flex items-center justify-between px-6">
+        <Disclosure as="nav" className="bg-white shadow-md">
+            {({ open }) => (
+                <>
+                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex items-center justify-between h-16">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex-shrink-0 flex items-center">
+                                    <img
+                                        className="block lg:hidden h-8 w-auto"
+                                        src="https://i.ibb.co/tDDkMFP/icon-stockroom-io-d129f9.png"
+                                        alt="Stock Room"
+                                    />
+                                   
+                                    <img
+                                        className="hidden lg:block h-8 w-auto"
+                                        src="https://i.ibb.co/tDDkMFP/icon-stockroom-io-d129f9.png"
+                                        alt="Stock Room"
+                                    />
+                                     <h2 className='text-gray-900 text-xl inline-block mx-1 font-bold'>Stock Room</h2>
+                                </div>
+                                {/* Laptop */}
+                                <div className="hidden sm:block sm:ml-6">
+                                  
+                                  <div className="flex justify-between">
+                                  <CustomLink to={'/'}>Home</CustomLink>
+                                   <CustomLink to={'/blog'}>Blog</CustomLink>
+                                    <CustomLink to={'/manageinventory'}>Manage Inventory</CustomLink>
+                                   {
+                                       user && <>
+                                       <CustomLink to={'/myitem'}>My Item</CustomLink>
+                                       <CustomLink to={'/manageinventory'}>Manage Inventory</CustomLink>
+                                       <CustomLink to={'/additem'}>Add Item</CustomLink>
+                                       
+                                       </>
+                                   }
+                                  </div>
+                                   </div>
+                                
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                               {
+                                   user ? 
+                                   <button className='px-2 p-1 bg-red-700 text-white rounded-md' onClick={()=>signOut(auth)}>Sign Out</button>
+                                    : <CustomLink to={'/login'}>Log In </CustomLink>
+                               }
 
+                                
+                            </div>
+                        </div>
+                    </div>
+                                                {/* Mobile*/}
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="px-2 pt-2 pb-3  ">
+                        <CustomLink  to={'/'}>Home</CustomLink>
+                        <CustomLink to={'/blog'}>Blog</CustomLink>
+                        <CustomLink to={'/manageinventory'}>Manage Inventory</CustomLink>
+                        {
+                                       user && <>
+                                       <CustomLink to={'/myitem'}>My Item</CustomLink>
+                                       <CustomLink to={'/manageinventory'}>Manage Inventory</CustomLink>
+                                       <CustomLink to={'/additem'}>Add Item</CustomLink>
+                                       
+                                       </>
+                                   }
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+    )
+}
 
-                <div className="flex items-center place-content-center" ><img className="w-10 h-10" src="https://i.ibb.co/tDDkMFP/icon-stockroom-io-d129f9.png" alt="" loading="lazy" />
-                    <h2 className="text-gray-900 font-bold text-xl ml-2">Stock Room</h2></div>
-
-                <div className="flex items-center place-content-center" >
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/blog'}>Blog</CustomLink>
-                   {
-                       !user ? 
-                       <> 
-                     
-                       <CustomLink to={'/login'}>Login</CustomLink>
-                       <CustomLink to={'/signup'}>Signup</CustomLink>
-                       </> 
-                       :
-                       <>
-                       <CustomLink to={'/manageinventory'}>Manage Items</CustomLink>
-                       <CustomLink to={`/myitem`}>My Item</CustomLink>
-                       <CustomLink to={'/additem'}>Add Item</CustomLink>
-                        <button className='bg-red-600 text-white font-bold rounded-md px-2 p-1' onClick={()=>signOut(auth)} >Log Out</button>
-                       </>
-                   }
-                </div>
-
-            </div>
-        </nav>
-
-    );
-};
-
-export default Header;
